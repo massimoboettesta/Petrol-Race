14bfa70c5f3926e7ca4b618a4f2d5bcebea12e61
Massimo <max_wapo97@hotmail.com>
2020-11-05 13:51:20 +0100

    Basic Multiplayer STARTED

---
+++ Petrol Race/Assets/UI/GetSteamName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GetSteamName : MonoBehaviour
+{
+    public TextMeshProUGUI TextRender;
+    public string SteamName;
+    void Start()
+    {
+        
+    }
+        
+    
+}
